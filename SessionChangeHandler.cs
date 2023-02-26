1d3b23385f39671bec613965ae12e93388b236df
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-02-26 03:06:30 +0100

    Added GetActiveConsoleSessionId

--- SessionChangeHandler.cs
+++ SessionChangeHandler.cs
@@ -17,250 +17,265 @@ namespace Nefarius.Utilities.SessionNotification;
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 public class SessionChangeHandler : IDisposable
 {
-    private readonly CancellationTokenSource _cancellationTokenSource = new();
+	private readonly CancellationTokenSource _cancellationTokenSource = new();
 
-    private readonly uint _flags;
+	private readonly uint _flags;
 
-    private readonly Thread _thread;
+	private readonly Thread _thread;
 
-    private HWND _windowHandle;
+	private HWND _windowHandle;
 
-    /// <summary>
-    ///     Creates a new session change listener.
-    /// </summary>
-    /// <param name="allSessions">True to listen to all sessions, false to only listen to the current session (default).</param>
-    public SessionChangeHandler(bool allSessions = false)
-    {
-        _flags = allSessions ? PInvoke.NOTIFY_FOR_ALL_SESSIONS : PInvoke.NOTIFY_FOR_THIS_SESSION;
+	/// <summary>
+	///     Creates a new session change listener.
+	/// </summary>
+	/// <param name="allSessions">True to listen to all sessions, false to only listen to the current session (default).</param>
+	public SessionChangeHandler(bool allSessions = false)
+	{
+		_flags = allSessions ? PInvoke.NOTIFY_FOR_ALL_SESSIONS : PInvoke.NOTIFY_FOR_THIS_SESSION;
 
-        _thread = new Thread(Start)
-        {
-            IsBackground = true
-        };
+		_thread = new Thread(Start)
+		{
+			IsBackground = true
+		};
 
-        _thread.Start();
-    }
+		_thread.Start();
+	}
 
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
 
-    /// <summary>
-    ///     A session was connected to the console terminal.
-    /// </summary>
-    public event Action<int> ConsoleConnect;
+	/// <summary>
+	///     A session was connected to the console terminal.
+	/// </summary>
+	public event Action<int> ConsoleConnect;
 
-    /// <summary>
-    ///     A session was disconnected from the console terminal.
-    /// </summary>
-    public event Action<int> ConsoleDisconnect;
+	/// <summary>
+	///     A session was disconnected from the console terminal.
+	/// </summary>
+	public event Action<int> ConsoleDisconnect;
 
-    /// <summary>
-    ///     A session was connected to the remote terminal.
-    /// </summary>
-    public event Action<int> RemoteConnect;
+	/// <summary>
+	///     A session was connected to the remote terminal.
+	/// </summary>
+	public event Action<int> RemoteConnect;
 
-    /// <summary>
-    ///     A session was disconnected from the remote terminal.
-    /// </summary>
-    public event Action<int> RemoteDisconnect;
+	/// <summary>
+	///     A session was disconnected from the remote terminal.
+	/// </summary>
+	public event Action<int> RemoteDisconnect;
 
-    /// <summary>
-    ///     A user has logged on to the session.
-    /// </summary>
-    public event Action<int> SessionLogon;
+	/// <summary>
+	///     A user has logged on to the session.
+	/// </summary>
+	public event Action<int> SessionLogon;
 
-    /// <summary>
-    ///     A user has logged off the session.
-    /// </summary>
-    public event Action<int> SessionLogoff;
+	/// <summary>
+	///     A user has logged off the session.
+	/// </summary>
+	public event Action<int> SessionLogoff;
 
-    /// <summary>
-    ///     A session has been locked.
-    /// </summary>
-    public event Action<int> SessionLock;
+	/// <summary>
+	///     A session has been locked.
+	/// </summary>
+	public event Action<int> SessionLock;
 
-    /// <summary>
-    ///     A session has been unlocked.
-    /// </summary>
-    public event Action<int> SessionUnlock;
+	/// <summary>
+	///     A session has been unlocked.
+	/// </summary>
+	public event Action<int> SessionUnlock;
 
-    /// <summary>
-    ///     A session has changed its remote controlled status.
-    /// </summary>
-    public event Action<int> SessionRemoteControl;
+	/// <summary>
+	///     A session has changed its remote controlled status.
+	/// </summary>
+	public event Action<int> SessionRemoteControl;
 
-    /// <summary>
-    ///     Resolves a username for a given session ID.
-    /// </summary>
-    /// <param name="sessionId">The session ID.</param>
-    /// <param name="prependDomain">True to prepend domain name, false to only return the username.</param>
-    /// <returns>The username.</returns>
-    public static unsafe string GetUsernameBySessionId(int sessionId, bool prependDomain = false)
-    {
-        var username = "SYSTEM";
+	/// <summary>
+	///     Resolves a username for a given session ID.
+	/// </summary>
+	/// <param name="sessionId">The session ID.</param>
+	/// <param name="prependDomain">True to prepend domain name, false to only return the username.</param>
+	/// <returns>The username.</returns>
+	public static unsafe string GetUsernameBySessionId(int sessionId, bool prependDomain = false)
+	{
+		var username = "SYSTEM";
 
-        if (PInvoke.WTSQuerySessionInformation(null, (uint)sessionId, WTS_INFO_CLASS.WTSUserName, out var buffer,
-                out var strLen) &&
-            strLen > 1)
-        {
-            username = new string(buffer);
-            PInvoke.WTSFreeMemory(buffer);
+		if (PInvoke.WTSQuerySessionInformation(null, (uint)sessionId, WTS_INFO_CLASS.WTSUserName, out var buffer,
+			    out var strLen) &&
+		    strLen > 1)
+		{
+			username = new string(buffer);
+			PInvoke.WTSFreeMemory(buffer);
 
-            if (prependDomain)
-                if (PInvoke.WTSQuerySessionInformation(null, (uint)sessionId, WTS_INFO_CLASS.WTSDomainName, out buffer,
-                        out strLen) && strLen > 1)
-                {
-                    username = new string(buffer) + "\\" + username;
-                    PInvoke.WTSFreeMemory(buffer);
-                }
-        }
+			if (prependDomain)
+				if (PInvoke.WTSQuerySessionInformation(null, (uint)sessionId, WTS_INFO_CLASS.WTSDomainName, out buffer,
+					    out strLen) && strLen > 1)
+				{
+					username = new string(buffer) + "\\" + username;
+					PInvoke.WTSFreeMemory(buffer);
+				}
+		}
 
-        return username;
-    }
+		return username;
+	}
 
-    private static string GenerateRandomString()
-    {
-        // Creating object of random class
-        var rand = new Random();
+	/// <summary>
+	///     Retrieves the session identifier of the console session. The console session is the session that is currently
+	///     attached to the physical console. Note that it is not necessary that Remote Desktop Services be running for this
+	///     function to succeed.
+	/// </summary>
+	/// <returns>
+	///     The session identifier of the session that is attached to the physical console. If there is no session
+	///     attached to the physical console, (for example, if the physical console session is in the process of being attached
+	///     or detached), this function returns 0xFFFFFFFF.
+	/// </returns>
+	public static uint GetActiveConsoleSessionId()
+	{
+		return PInvoke.WTSGetActiveConsoleSessionId();
+	}
 
-        // Choosing the size of string
-        // Using Next() string
-        var stringlen = rand.Next(4, 10);
-        int randValue;
-        var sb = new StringBuilder();
-        char letter;
-        for (var i = 0; i < stringlen; i++)
-        {
-            // Generating a random number.
-            randValue = rand.Next(0, 26);
+	private static string GenerateRandomString()
+	{
+		// Creating object of random class
+		var rand = new Random();
 
-            // Generating random character by converting
-            // the random number into character.
-            letter = Convert.ToChar(randValue + 65);
+		// Choosing the size of string
+		// Using Next() string
+		var stringlen = rand.Next(4, 10);
+		int randValue;
+		var sb = new StringBuilder();
+		char letter;
+		for (var i = 0; i < stringlen; i++)
+		{
+			// Generating a random number.
+			randValue = rand.Next(0, 26);
 
-            // Appending the letter to string.
-            sb.Append(letter);
-        }
+			// Generating random character by converting
+			// the random number into character.
+			letter = Convert.ToChar(randValue + 65);
 
-        return sb.ToString();
-    }
+			// Appending the letter to string.
+			sb.Append(letter);
+		}
 
-    private unsafe void Start(object parameter)
-    {
-        var className = GenerateRandomString(); // random string to avoid conflicts
-        var windowName = GenerateRandomString();
-        using var hInst = PInvoke.GetModuleHandle((string)null);
+		return sb.ToString();
+	}
 
-        var windowClass = new WNDCLASSEXW
-        {
-            cbSize = (uint)Marshal.SizeOf<WNDCLASSEXW>(),
-            style = WNDCLASS_STYLES.CS_HREDRAW | WNDCLASS_STYLES.CS_VREDRAW,
-            cbClsExtra = 0,
-            cbWndExtra = 0,
-            hInstance = (HINSTANCE)hInst.DangerousGetHandle(),
-            lpfnWndProc = WndProc
-        };
+	private unsafe void Start(object parameter)
+	{
+		var className = GenerateRandomString(); // random string to avoid conflicts
+		var windowName = GenerateRandomString();
+		using var hInst = PInvoke.GetModuleHandle((string)null);
 
-        fixed (char* pClassName = className)
-        fixed (char* pWindowName = windowName)
-        {
-            windowClass.lpszClassName = pClassName;
+		var windowClass = new WNDCLASSEXW
+		{
+			cbSize = (uint)Marshal.SizeOf<WNDCLASSEXW>(),
+			style = WNDCLASS_STYLES.CS_HREDRAW | WNDCLASS_STYLES.CS_VREDRAW,
+			cbClsExtra = 0,
+			cbWndExtra = 0,
+			hInstance = (HINSTANCE)hInst.DangerousGetHandle(),
+			lpfnWndProc = WndProc
+		};
 
-            PInvoke.RegisterClassEx(windowClass);
+		fixed (char* pClassName = className)
+		fixed (char* pWindowName = windowName)
+		{
+			windowClass.lpszClassName = pClassName;
 
-            _windowHandle = PInvoke.CreateWindowEx(
-                0,
-                pClassName,
-                pWindowName,
-                0,
-                0, 0, 0, 0,
-                HWND.Null,
-                HMENU.Null,
-                new HINSTANCE(hInst.DangerousGetHandle())
-            );
-        }
+			PInvoke.RegisterClassEx(windowClass);
 
-        if (!PInvoke.WTSRegisterSessionNotification(_windowHandle, _flags))
-            Marshal.ThrowExceptionForHR(Marshal.GetLastWin32Error());
+			_windowHandle = PInvoke.CreateWindowEx(
+				0,
+				pClassName,
+				pWindowName,
+				0,
+				0, 0, 0, 0,
+				HWND.Null,
+				HMENU.Null,
+				new HINSTANCE(hInst.DangerousGetHandle())
+			);
+		}
 
-        MessagePump();
-    }
+		if (!PInvoke.WTSRegisterSessionNotification(_windowHandle, _flags))
+			Marshal.ThrowExceptionForHR(Marshal.GetLastWin32Error());
 
-    private void MessagePump()
-    {
-        int retVal;
-        while ((retVal = PInvoke.GetMessage(out var msg, HWND.Null, 0, 0)) != 0 &&
-               !_cancellationTokenSource.Token.IsCancellationRequested)
-            if (retVal == -1)
-            {
-                break;
-            }
-            else
-            {
-                PInvoke.TranslateMessage(msg);
-                PInvoke.DispatchMessage(msg);
-            }
-    }
+		MessagePump();
+	}
 
-    private LRESULT WndProc(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
-    {
-        if (msg == PInvoke.WM_WTSSESSION_CHANGE)
-        {
-            switch (wParam.Value)
-            {
-                case PInvoke.WTS_CONSOLE_CONNECT:
-                    ConsoleConnect?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_CONSOLE_DISCONNECT:
-                    ConsoleDisconnect?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_REMOTE_CONNECT:
-                    RemoteConnect?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_REMOTE_DISCONNECT:
-                    RemoteDisconnect?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_SESSION_LOGON:
-                    SessionLogon?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_SESSION_LOGOFF:
-                    SessionLogoff?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_SESSION_LOCK:
-                    SessionLock?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_SESSION_UNLOCK:
-                    SessionUnlock?.Invoke((int)lParam.Value);
-                    break;
-                case PInvoke.WTS_SESSION_REMOTE_CONTROL:
-                    SessionRemoteControl?.Invoke((int)lParam.Value);
-                    break;
-            }
+	private void MessagePump()
+	{
+		int retVal;
+		while ((retVal = PInvoke.GetMessage(out var msg, HWND.Null, 0, 0)) != 0 &&
+		       !_cancellationTokenSource.Token.IsCancellationRequested)
+			if (retVal == -1)
+			{
+				break;
+			}
+			else
+			{
+				PInvoke.TranslateMessage(msg);
+				PInvoke.DispatchMessage(msg);
+			}
+	}
 
-            return new LRESULT(0);
-        }
+	private LRESULT WndProc(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
+	{
+		if (msg == PInvoke.WM_WTSSESSION_CHANGE)
+		{
+			switch (wParam.Value)
+			{
+				case PInvoke.WTS_CONSOLE_CONNECT:
+					ConsoleConnect?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_CONSOLE_DISCONNECT:
+					ConsoleDisconnect?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_REMOTE_CONNECT:
+					RemoteConnect?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_REMOTE_DISCONNECT:
+					RemoteDisconnect?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_SESSION_LOGON:
+					SessionLogon?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_SESSION_LOGOFF:
+					SessionLogoff?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_SESSION_LOCK:
+					SessionLock?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_SESSION_UNLOCK:
+					SessionUnlock?.Invoke((int)lParam.Value);
+					break;
+				case PInvoke.WTS_SESSION_REMOTE_CONTROL:
+					SessionRemoteControl?.Invoke((int)lParam.Value);
+					break;
+			}
 
-        return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
-    }
+			return new LRESULT(0);
+		}
 
-    private void ReleaseUnmanagedResources()
-    {
-        PInvoke.WTSUnRegisterSessionNotification(_windowHandle);
-        PInvoke.DestroyWindow(_windowHandle);
-    }
+		return PInvoke.DefWindowProc(hwnd, msg, wParam, lParam);
+	}
 
-    private void Dispose(bool disposing)
-    {
-        ReleaseUnmanagedResources();
-        if (disposing) _cancellationTokenSource?.Dispose();
-        _thread.Join();
-    }
+	private void ReleaseUnmanagedResources()
+	{
+		PInvoke.WTSUnRegisterSessionNotification(_windowHandle);
+		PInvoke.DestroyWindow(_windowHandle);
+	}
 
-    ~SessionChangeHandler()
-    {
-        Dispose(false);
-    }
+	private void Dispose(bool disposing)
+	{
+		ReleaseUnmanagedResources();
+		if (disposing) _cancellationTokenSource?.Dispose();
+		_thread.Join();
+	}
+
+	~SessionChangeHandler()
+	{
+		Dispose(false);
+	}
 }
